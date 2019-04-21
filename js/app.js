b102c90f60c6439dc983a3f4a74efe646139dabf
Blue Circus <wictornisa@hotmail.com>
2019-04-21 20:55:00 +0200

    added quotes

--- js/app.js
+++ js/app.js
@@ -15,7 +15,19 @@ let randomSaying = ["I hope you're pleased with yourselves. We could all have be
 
  "It takes a great deal of bravery to stand up to our enemies, but just as much to stand up to our friends. ― Albus Dumbledore, Harry Potter and the Sorcerer's Stone",
 
- "“Yer a wizard Harry.” ― Hagrid, Harry Potter and the Sorcerer's Stone"
+ "“Yer a wizard Harry.” ― Hagrid, Harry Potter and the Sorcerer's Stone",
+
+ "“I solemnly swear I am up to no good.” ― Harry Potter, Harry Potter and the Prisoner of Azkaban",
+
+ "Honestly, am I the only person who's ever bother to read Hogwarts: A History?' — Hermione Granter, Harry Potter and the Prisoner of Azkaban",
+
+ "“But you know, happiness can be found even in the darkest of times, if one only remembers to turn on the light.” ― Albus Dumbledore, Harry Potter and the Prisoner of Azkaban",
+
+ "If you want to know what a man’s like, take a good look at how he treats his inferiors, not his equals. — Sirius Black, Harry Potter and the Goblet of Fire",
+
+ "“Things we lose have a way of coming back to us in the end, if not always in the way we expect.”  ― Luna Lovegood, Harry Potter and the Order of the Phoenix.",
+
+ "I mean, it's sort of exciting, isn't it, breaking the rules? — Hermione Granger, Harry Potter and the Order of the Phoenix"
 ];
 
 
